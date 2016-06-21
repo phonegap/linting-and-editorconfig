08e8a37f4bc11a7eec9f66818bca2d62903784e3
tommy-carlos williams <tommy@devgeeks.org>
2016-06-22 08:03:54 +1000

    Added some example failures and pretest scripts

---
+++ airbnb-customised/src/index.js
@@ -0,0 +1,12 @@
+// Unused variable
+// Single quotes
+const foo = 'bar';
+
+// Incorrect indentation
+// Use of var
+// Double quotes are fine
+    var something = "somethingelse";
+
+// console statement is fine here...
+// No semicolon
+console.log(something)
---
+++ airbnb/src/index.js
@@ -0,0 +1,11 @@
+// Unused variable
+const foo = 'bar';
+
+// Incorrect indentation
+// Use of var
+// Double quotes
+    var something = "somethingelse";
+
+// console statement
+// No semicolon
+console.log(something)
